f72ca3eeef6042eabaa986af12f5c4b1fb96993d
ducatiqwq <2749458239@qq.com>
2026-07-29 03:01:05 +0000

    fix hopper/flash_attn_3_source issue

--- hopper/flash_attn_3/flash_attn_interface.py
+++
@@ -1,9 +0,0 @@
-# Alias module so that `from flash_attn_3.flash_attn_interface import ...` works.
-from flash_attn_interface import *  # noqa: F401,F403
-from flash_attn_interface import (  # noqa: F401
-    flash_attn_func,
-    flash_attn_varlen_func,
-    flash_attn_with_kvcache,
-    flash_attn_combine,
-    get_scheduler_metadata,
-)
